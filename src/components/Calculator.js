4a0ff04ed2a99fda75fbd20008f1c57b2b54d9d1
mullenslucas <mullenlucas@gmail.com>
2022-09-16 13:04:07 -0300

    transform to ES6 and adjustments

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,52 @@
 /* eslint-disable */
-// const Items = (props) => {
-//   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
-// }
+import React from 'react';
 
-// Items.defaultProps = {
-//   quantity: 0
-// }
+const Numa = (props) => {
+  return <input type="number" id="calc-curr-num" defaultValue={props.quantity} name="calc-curr-num"/>
+}
 
-// class Calculator extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//         
-//   }
-// };
+Numa.defaultProps = {
+  quantity: 0
+}
 
-function Calculator () {
-  return (
-    <section id="calculator-sec">
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    return (
+      <section id="calculator-sec">
 
-      <div className="calculator">
-        <div className="calc-num">
-          <input type="text" id="lname" placeholder="HOLIS" name="lname"/>
+        <div className="calculator">
+          <div className="calc-num">
+            <Numa/>
+          </div>
+
+          <div className="calc-buttons">
+            <button type="button" className="calc-btn orange">AC</button>
+            <button type="button" className="calc-btn orange">+/-</button>
+            <button type="button" className="calc-btn orange">%</button>
+            <button type="button" className="calc-btn green">÷</button>
+            <button type="button" className="calc-btn">7</button>
+            <button type="button" className="calc-btn">8</button>
+            <button type="button" className="calc-btn">9</button>
+            <button type="button" className="calc-btn green">x</button>
+            <button type="button" className="calc-btn">4</button>
+            <button type="button" className="calc-btn">5</button>
+            <button type="button" className="calc-btn">6</button>
+            <button type="button" className="calc-btn green">-</button>
+            <button type="button" className="calc-btn">1</button>
+            <button type="button" className="calc-btn">2</button>
+            <button type="button" className="calc-btn">3</button>
+            <button type="button" className="calc-btn green">+</button>
+            <button type="button" className="calc-btn span-2">0</button>
+            <button type="button" className="calc-btn">.</button>
+            <button type="button" className="calc-btn green">=</button>
+          </div>
         </div>
-
-        <div className="calc-buttons">
-          <button type="button" className="calc-btn orange">AC</button>
-          <button type="button" className="calc-btn orange">+/-</button>
-          <button type="button" className="calc-btn orange">%</button>
-          <button type="button" className="calc-btn green">÷</button>
-          <button type="button" className="calc-btn">7</button>
-          <button type="button" className="calc-btn">8</button>
-          <button type="button" className="calc-btn">9</button>
-          <button type="button" className="calc-btn green">x</button>
-          <button type="button" className="calc-btn">4</button>
-          <button type="button" className="calc-btn">5</button>
-          <button type="button" className="calc-btn">6</button>
-          <button type="button" className="calc-btn green">-</button>
-          <button type="button" className="calc-btn">1</button>
-          <button type="button" className="calc-btn">2</button>
-          <button type="button" className="calc-btn">3</button>
-          <button type="button" className="calc-btn green">+</button>
-          <button type="button" className="calc-btn span-2">0</button>
-          <button type="button" className="calc-btn">.</button>
-          <button type="button" className="calc-btn green">=</button>
-        </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
 
 export default Calculator;

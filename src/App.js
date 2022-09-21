2e9ab99803c7381103e93989202d6f45bfb38110
mullenslucas <mullenlucas@gmail.com>
2022-09-21 17:45:02 -0300

    transform App from class-based to function

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <main id="main-section">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main id="main-section">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
